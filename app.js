486328886b4a17c490ad2ed8b564b01bbc453c7d
Anjala <anjaladilhara@gmail.com>
2020-10-18 10:08:04 +0530

    basic functions added

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+require('body-parser-xml')(bodyParser);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(bodyParser.xml({
+  limit: '1MB',   // Reject payload bigger than 1 MB
+  xmlParseOptions: {
+    normalize: true,     // Trim whitespace inside text nodes
+    normalizeTags: true, // Transform tags to lowercase
+    explicitArray: false // Only put nodes in array if >1
+  }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +34,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,82 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+
+var mysql = require('mysql');
+const config = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  var connection = mysql.createConnection(config);
+  connection.connect();
+  connection.query('SELECT * FROM data ORDER BY collected;', function (err, results, fields) {
+    if (err) throw err
+
+    if (results) {
+      console.log(results);
+      var processed = {
+        temp: [],
+        humidity: [],
+        pressure: [],
+        light: [],
+      }
+      results.forEach(({ id, temp, humidity, pressure, light, collected }) => {
+        collected = new Date(collected).getTime();
+        processed.temp.push({ x: collected, y: temp });
+        processed.humidity.push({ x: collected, y: humidity });
+        processed.pressure.push({ x: collected, y: pressure });
+        processed.light.push({ x: collected, y: light });
+      });
+
+      res.render('index', { title: 'Sensor Data', data: processed });
+    }
+  })
+  connection.end();
+
+});
+
+
+// store data received from sensors
+router.post('/push', function (req, res) {
+  const values = req.body.params.value;
+  let processed = "";
+  try {
+    if (Array.isArray(values)) {
+      const date = moment();
+      values.reverse();
+      values.forEach((element) => {
+        const { humidity, temperature, light } = element;
+        processed = '("' + date.toISOString() + '", ' + humidity + ', ' + light + ', NULL,' + temperature + '),' + processed;
+        date.subtract({ seconds: 30 });
+      });
+    } else {
+      const { humidity, temperature, light } = values;
+      processed = '("' + moment().toISOString() + '", ' + humidity + ', ' + light + ', NULL, ' + temperature + '),';
+    }
+  } catch (e) {
+    console.error(e);
+  }
+  processed = processed.slice(0, -1);
+  // console.log(values);
+  console.log(processed);
+  const connection = mysql.createConnection(config);
+  connection.connect();
+  const query = 'INSERT INTO data (collected, humidity, light, pressure, temp) VALUES' + processed + ';';
+  connection.query(
+    query,
+    function (err) {
+      if (err) throw err
+    })
+  connection.end();
+
+  res.status(200);
+  res.send();
 });
 
 module.exports = router;
